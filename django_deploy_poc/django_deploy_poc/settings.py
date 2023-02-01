a766fbb4825279b1bedaf03b72e2ff2b740504a2
Ketansingh Patil <ketanpatil@rover-247-99.rovernet.mtu.edu>
2023-02-01 15:39:53 -0500

    commit changes - changed settings.py

--- django_deploy_poc/django_deploy_poc/settings.py
+++ django_deploy_poc/django_deploy_poc/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -115,3 +116,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Make sure that BASE_DIR is defined somewhere at the top
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
